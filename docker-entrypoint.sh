b6a3cbb4b2da0ecd1e533df3049b3d262ad36a67
usr <omgoo191@gmail.com>
2026-05-18 11:11:58 +0300

    :(

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -e
-
 echo "Waiting for postgres..."
 until pg_isready -h postgres -U radar; do
   sleep 1
 done
-
 echo "Running migration"
 python scripts/migrate.py
-
-echo "starting radar"
-python src/main.py
+echo "starting radar in mode: ${RADAR_MODE:-all}"
+python src/main.py ${RADAR_MODE:-all}
